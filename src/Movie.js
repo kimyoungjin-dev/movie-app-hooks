5e969754a6dcf6e3cdef4ddb5300b2f47cfdd04f
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-17 00:48:10 +0900

    Add prop-types and display movie information on the screen

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Movie = ({ data }) => {
-  return <div>무비</div>;
+const Movie = ({ title, genres, summary, year, poster, id }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h2>{genres}</h2>
+      <h2>{summary.slice(0, 180)}...</h2>
+      <h2>{year}</h2>
+      <img src={poster} />
+    </div>
+  );
+};
+
+Movie.PropTypes = {
+  title: PropTypes.string.isRequired,
+  genres: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
+  poster: PropTypes.string.isRequired,
 };
 
 export default Movie;
